6fe58ba81e1e160fa7c48988b7b096cf396ebe54
Clailton F. Silva <claybom24@gmail.com>
2025-08-17 17:42:32 -0300

    Function sub-project added...

---
+++ Resume/Function/main.cpp
@@ -0,0 +1,33 @@
+#include "vld.h" // No memory leaks DETECTED!
+#include <iostream>
+#include "default_arguments.h"
+#include "inline_function.h"
+#include "overloading.h"
+#include "redefining.h"
+#include "var_params.h"
+#include "virtual.h"
+using namespace std;
+
+// Prototype:
+long multiply(long, long); // Optional var names
+
+int main() {
+
+	cout << "2 x 3 = " << multiply(2, 3) << endl;
+	_overloading();
+	_redefining();
+	_var_params();
+	_virtual();
+	_default_arguments();
+	_inlineFunctionUsage();
+
+	std::cout << "\n\n\n";
+	// Leak memory test:
+	//int *ptr = new int(33);
+	system("pause");
+	return(0);
+}
+
+static long multiply(long left, long right) { // Var names are optional if not used inside func body. Otherwise how would ya reference them?
+	return (long)left * right;
+}
