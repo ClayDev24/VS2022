3540a96c18c10764f2baeb908270a4e7a1b9ed55
Clailton F. Silva <claybom24@gmail.com>
2025-08-17 17:02:10 -0300

    Enum sub-project added...

---
+++ Resume/Enum/anonymous.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+#include <iostream>
+
+namespace resume {
+
+	// Global anonymous union must be static (internal linkage):
+	static union {
+		char c;
+		long l;
+	};
+
+	static void _anonymous() {
+		// Anonymous Union:
+		union { // no type name...
+			int   i;
+			float f;
+		}; // ...no variables name declared
+		// Access members w/out usin' qualifiers:
+		i = 12; // Access as ordinary variables
+		std::cout << "i: " << i << std::endl;
+		f = 1.22f; // W/out dot operator
+		std::cout << "f: " << f << std::endl;
+		// Union type access mismatch:
+		std::cout << "i: " << i << "...type access mismatch(float as int)\n";
+	}
+} // resume
