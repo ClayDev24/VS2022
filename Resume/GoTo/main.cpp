ec3db84b50c578bce1cde9dc41e36c8175d40f0b
Clailton F. Silva <claybom24@gmail.com>
2025-08-18 10:20:01 -0300

    GoTo sub-project added...

---
+++ Resume/GoTo/main.cpp
@@ -0,0 +1,26 @@
+// The infamous goto is supported in C++
+#include "vld.h" // No memory leaks DETECTED!
+#include <iostream>
+#include <conio.h> // _getche()
+using namespace std;
+
+int main() {
+
+top:
+	cout << "Countin test...\n";
+	for (int i = 0; i < 10; i++)
+		cout << "i=> " << i << endl;
+
+	char ch = 'a';
+	while (toupper(ch) != 'N') {
+		std::cout << "again? (y/n):\n";
+		ch = toupper(_getche());
+		if ((ch) == 'Y') goto top;
+	}
+
+	std::cout << "\n\n\n";
+	// Leak memory test:
+	//int *ptr = new int(33);
+	system("pause");
+	return(0);
+}
